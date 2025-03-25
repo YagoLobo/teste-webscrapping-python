207ab104ec4e22f81396692132a47abd48bd24e5
Yago <yagolobo.dev@gmail.com>
2025-03-25 04:12:13 -0300

    feat:added second question of the test and renamed first test file

---
+++ question2.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import pdfplumber
+import zipfile
+import os
+from pathlib import Path
+
+all_tables = []
+
+with pdfplumber.open("Downloads/pdfs/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf") as pdf:
+    for page in pdf.pages[2:]:
+        tables = page.extract_tables()
+        print(f"extracting page:{page}")
+        for table in tables:
+            header = table[0]
+            rows =  table[1:]
+            page_dataframe = pd.DataFrame(rows, columns=header)
+            all_tables.append(page_dataframe)
+
+
+final_dataframe = pd.concat(all_tables, ignore_index=True)
+final_dataframe.rename(columns={
+    "OD": "Seg. Odontológica",
+    "AMB": "Seg. Ambulatorial"
+}, inplace=True)
+final_dataframe.to_csv("question2.csv", index=False)
+
+
+output_dir = Path(os.getcwd())/"Downloads/extracted_data"
+output_dir.mkdir(exist_ok=True) 
+
+
+zip_path = output_dir / "Teste_Yago_Lobo.zip"
+with zipfile.ZipFile(zip_path, 'w') as zipf:
+    zipf.write("question2.csv")
